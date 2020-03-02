242d730986a736f66657726e8f42da3a3a9cb0eb
maximtrp <maximtrp@gmail.com>
2020-03-02 22:08:52 +0300

    weather upd

--- weather_cpp/README.md
+++ weather_cpp/README.md
@@ -1,12 +1,12 @@
 # Weather blocklet
 
-This blocklet retrieves and shows current weather and forecast from OpenWeatherMap.
+This is a C++ version of my weather blocklet. It includes two implementations based on [nlohmann/json](https://github.com/nlohmann/json) and [rapidjson](https://github.com/Tencent/rapidjson) libraries. The first one is blocklet retrieves and shows current weather and forecast from OpenWeatherMap.
 
 ![Screenshot](screen.png)
 
 ## Requirements
 
-* Golang compiler
+* C++ compiler
 
 * API key for [OpenWeatherMap](https://openweathermap.org/api)
 
@@ -15,7 +15,7 @@ This blocklet retrieves and shows current weather and forecast from OpenWeatherM
 1. Compile the blocklet using `make`:
 
 ```bash
-make build
+make
 ```
 
 2. Copy the compiled file to your blockets directory (`/usr/lib/i3blocks`, in my case):
---
+++ weather_cpp/nlohmann-json/weather.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <map>
+#include <algorithm>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+std::map<std::string, std::string> weather_icons = {
+    {"clear", ""},
+    {"cloud", ""},
+    {"rain", ""},
+    {"storm", ""},
+    {"snow", ""},
+    {"mist", ""},
+    {"fog", ""},
+    {"smoke", ""}
+};
+
+static size_t write_callback(void *contents, size_t size, size_t nmemb, void *buf) {
+
+    ((std::string*)buf)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+
+}
+
+json get_data(std::string *url) {
+
+    std::string response_buffer;
+    CURL *curl_handle;
+    CURLcode curl_result;
+    curl_handle = curl_easy_init();
+
+    if (curl_handle) {
+        curl_easy_setopt(curl_handle, CURLOPT_URL, url->c_str());
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &response_buffer);
+        curl_result = curl_easy_perform(curl_handle);
+        curl_easy_cleanup(curl_handle);
+    }
+
+    return json::parse(response_buffer);
+
+}
+
+std::string get_icon(std::string weather) {
+    std::string weather_lower = weather;
+    std::for_each(weather_lower.begin(), weather_lower.end(), [](char &c){
+	    c = ::tolower(c);
+    });
+
+    std::string icon = "";
+    for (auto const& weather_item : weather_icons) {
+        if (weather_lower.find(weather_item.first) != std::string::npos) {
+            icon = weather_item.second;
+            break;
+        }
+    }
+    return icon;
+}
+
+std::string get_weather_string(const json &weather) {
+    std::string weather_main = weather["weather"][0]["main"].get<std::string>();
+    std::string icon = get_icon(weather_main);
+    std::string temp = std::to_string(std::lrint(weather["main"]["temp"].get<double>()));
+    return icon + " " + temp + "°";
+}
+
+std::string get_forecast_string(const json& forecast) {
+
+    std::string forecast_str = "";
+    long int now = static_cast<long int>(std::time(nullptr));
+
+    for (auto const forecast_item : forecast["list"]) {
+        if (forecast_item["dt"].get<long int>() - now > 60*60*6) {
+            forecast_str = get_weather_string(forecast_item);
+            break;
+        }
+    }
+
+    return forecast_str;
+}
+
+int main(void) {
+
+    char *city = std::getenv("CITY");
+    char *apikey = std::getenv("APIKEY");
+    char *units = std::getenv("UNITS");
+
+    if (city == nullptr || apikey == nullptr || units == nullptr) {
+        return 1;
+    }
+
+    std::string city_str(city, strlen(city));
+    std::string apikey_str(apikey, strlen(apikey));
+    std::string units_str(units, strlen(units));
+
+    std::string weather_url = "http://api.openweathermap.org/data/2.5/weather";
+    weather_url += "?id=" + city_str + "&appid=" + apikey_str + "&units=" + units_str;
+    
+    std::string forecast_url = "http://api.openweathermap.org/data/2.5/forecast";
+    forecast_url += "?id=" + city_str + "&appid=" + apikey_str + "&units=" + units_str;
+
+    json weather = get_data(&weather_url);
+    std::string weather_str = get_weather_string(weather);
+
+    json forecast = get_data(&forecast_url);
+    std::string forecast_str = get_forecast_string(forecast);
+
+    std::cout << weather_str;
+    std::cout << " → ";
+    std::cout << forecast_str << std::endl;
+    return 0;
+}
